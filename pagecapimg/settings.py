6ad6d87e76deb6075195ee2117c0974a6b480b5f
idelfrides <idelfridesjorgepapai@gmail.com>
2022-06-07 17:30:18 -0300

    upload a new image and change media position

--- pagecapimg/settings.py
+++ pagecapimg/settings.py
@@ -135,17 +135,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-STATIC_URL = '/static_cdn/'
+STATIC_URL = '/static/'
+# STATIC_URL = '/static_cdn/'
 
 STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static_cdn"),
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static_cdn"),
     # '/var/www/static/'
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-# STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 
 # ---------------------------------------------------------
